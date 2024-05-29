7de8c1f6b8d7ef63a1b9a31b0bfcc523372f0ab7
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2024-05-29 11:10:42 +0200

    feat: implement move events for Robot

--- src/main/scala/robotswarm/model/Robot.scala
+++ src/main/scala/robotswarm/model/Robot.scala
@@ -3,4 +3,15 @@ package robotswarm.model
 import utils.Position
 import utils.Direction
 
-case class Robot(id: Int)(var position: Position)(var direction: Direction)
+case class Robot(id: Int)(var pos: Position)(var dir: Direction):
+    def move(): Unit =
+        pos = dir match
+            case Direction.North => pos.copy(y = pos.y + 1)
+            case Direction.East => pos.copy(x = pos.x + 1)
+            case Direction.South => pos.copy(y = pos.y - 1)
+            case Direction.West => pos.copy(x = pos.x - 1)
+
+    def setDirection(dest: Direction): Unit =
+        dir = dest
+end Robot
+
--- src/test/scala/robotswarm/Robot.scala
+++ src/test/scala/robotswarm/Robot.scala
@@ -9,18 +9,18 @@ import utils.Direction
 class RobotSimulatorSpec extends AnyFlatSpec with Matchers {
     "A Robot" should "be created correctly" in {
         val robot = new Robot(1)(Position(0, 0))(Direction.North)
-        robot.position should be(Position(0, 0))
-        robot.direction should be(Direction.North)
+        robot.pos should be(Position(0, 0))
+        robot.dir should be(Direction.North)
     } 
 
-    // "A Robot " should "correctly handle move events" in {
-    //     val robot = new Robot(1)(Position(0, 0))(Direction.North)
-    //     robot.move()
-    //     robot.position should be(Position(0, 1))
-    //     robot.direction should be(Direction.North)
-    //     robot.setDirection(Direction.East)
-    //     robot.direction should be (Direction.East)
-    //     robot.move()
-    //     robot.position should be(Position(1, 1))
-    // }
+    "A Robot" should "correctly handle move events" in {
+        val robot = new Robot(1)(Position(0, 0))(Direction.North)
+        robot.move()
+        robot.pos should be(Position(0, 1))
+        robot.dir should be(Direction.North)
+        robot.setDirection(Direction.East)
+        robot.dir should be (Direction.East)
+        robot.move()
+        robot.pos should be(Position(1, 1))
+    }
 }

cef3f105256ea7394eada9ed6386d957d49dcb3a
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2024-05-29 11:37:10 +0200

    feat: implement position control for environment

--- src/main/scala/robotswarm/model/Environment.scala
+++ src/main/scala/robotswarm/model/Environment.scala
@@ -1,8 +1,15 @@
 package robotswarm.model
 
+import utils.Position
+
 trait Grid:
     val width: Int
     val height: Int
 end Grid
 
-case class Environment (val width: Int, val height: Int) extends Grid
+case class Environment (val width: Int, val height: Int) extends Grid:
+    private val grid = Array.ofDim[Option[Robot]](width, height)
+
+    def isPositionValid(pos: Position): Boolean =
+        val (x, y) = (pos.x, pos.y)
+        x >= 0 && x < width && y >= 0 && y < height
--- src/test/scala/robotswarm/Environment.scala
+++ src/test/scala/robotswarm/Environment.scala
@@ -3,11 +3,20 @@ package robotswarm
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import robotswarm.model.Environment
+import utils.Position
 
 class EnvironmentSpec extends AnyFlatSpec with Matchers {
     "An Environment" should "be created correctly" in {
         val env = new Environment(10, 10)
         env.width should be(10)
         env.height should be(10)
-    } 
+    }
+
+    it should "have a valid position" in {
+        val env = new Environment(10, 10)
+        env.isPositionValid(Position(0, 0)) should be(true)
+        env.isPositionValid(Position(9, 9)) should be(true)
+        env.isPositionValid(Position(10, 10)) should be(false)
+        env.isPositionValid(Position(-1, -1)) should be(false)
+    }
 }

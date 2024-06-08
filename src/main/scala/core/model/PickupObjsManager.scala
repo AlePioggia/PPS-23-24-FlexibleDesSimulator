63871bc77164fe3148cd299e116f88665623f3dc
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2024-06-08 12:08:15 +0200

    feat: complete pickupObjsManager implementation

--- src/main/scala/core/model/PickupObjsManager.scala
+++ src/main/scala/core/model/PickupObjsManager.scala
@@ -8,3 +8,9 @@ class PickupObjsManager(val width: Int, val height: Int):
     def addObject(pos: Position): Unit = pickupObjs(pos.x)(pos.y) = true
     def removeObject(pos: Position): Unit = pickupObjs(pos.x)(pos.y) = false
     def isObjectAt(pos: Position): Boolean = pickupObjs(pos.x)(pos.y)
+    def objsPosList: List[Position] = 
+        (for
+            x <- 0 until width
+            y <- 0 until height
+            if pickupObjs(x)(y)
+        yield Position(x, y)).toList
--- src/test/scala/core/PickupObjsManager.scala
+++ src/test/scala/core/PickupObjsManager.scala
@@ -2,6 +2,7 @@ package core
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import utils.Position
 
 class PickupObjsManager extends AnyFlatSpec with Matchers {
     it should "add an object correctly" in {
@@ -16,5 +17,15 @@ class PickupObjsManager extends AnyFlatSpec with Matchers {
         manager.isObjectAt(utils.Position(0, 0)) should be(true)
         manager.removeObject(utils.Position(0, 0))
         manager.isObjectAt(utils.Position(0, 0)) should be(false)
+    }
+
+    it should "return objects as a list of positions" in {
+        val manager = new core.model.PickupObjsManager(10, 10)
+        manager.addObject(Position(0, 0))
+        manager.addObject(Position(0, 1))
+        manager.addObject(Position(1, 0))
+        manager.addObject(Position(1, 1))
+        manager.objsPosList should contain theSameElementsAs 
+            List(Position(0, 0), Position(0, 1), Position(1, 0), Position(1, 1))
     }
 }

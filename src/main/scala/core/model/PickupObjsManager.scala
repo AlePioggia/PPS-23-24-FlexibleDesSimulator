db687ea3904bf7cf0652dd181d304fc11b610cd1
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2024-06-08 12:10:09 +0200

    chore: refactor

--- src/main/scala/core/model/PickupObjsManager.scala
+++ src/main/scala/core/model/PickupObjsManager.scala
@@ -6,8 +6,11 @@ class PickupObjsManager(val width: Int, val height: Int):
     private val pickupObjs = Array.fill[Boolean](width, height)(false)
 
     def addObject(pos: Position): Unit = pickupObjs(pos.x)(pos.y) = true
+    
     def removeObject(pos: Position): Unit = pickupObjs(pos.x)(pos.y) = false
+    
     def isObjectAt(pos: Position): Boolean = pickupObjs(pos.x)(pos.y)
+    
     def objsPosList: List[Position] = 
         (for
             x <- 0 until width

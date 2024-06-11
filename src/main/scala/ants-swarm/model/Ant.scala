c4115abf92781662ef73387378cdc954e69c9873
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2024-06-11 15:26:59 +0200

    feat: implement food picking for ants

--- src/main/scala/ants-swarm/model/Ant.scala
+++ src/main/scala/ants-swarm/model/Ant.scala
@@ -5,5 +5,9 @@ import utils.Position
 import utils.Direction
 
 class Ant(val id: Int, var pos: Position, var dir: Direction, val nestPos: Position) extends Agent {
-    
+    var carryingFood: Boolean = false
+
+    override def interactWithObject(): Unit = 
+        if carryingFood then if pos == nestPos then carryingFood = false
+        else carryingFood = true
 }
--- src/test/scala/ants-swarm/Ant.scala
+++ src/test/scala/ants-swarm/Ant.scala
@@ -1,4 +1,4 @@
-package `ants-swarm`
+package `antsswarm`
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
@@ -12,4 +12,23 @@ class AntSpec extends AnyFlatSpec with Matchers {
         ant.pos should be(Position(0, 0))
         ant.dir should be(Direction.North)
     }
+
+    it should "be dropping its food when he reaches the nest position" in {
+        val ant = Ant(1, Position(0, 0), Direction.South, Position(0, 2))
+        ant.carryingFood = true
+        ant.move()
+        ant.carryingFood should be(true)
+        ant.move()
+        ant.interactWithObject()
+        ant.carryingFood should be(false)
+    }
+
+    it should "not pick up food when already carrying food" in {
+        val ant = Ant(1, Position(0, 0), Direction.South, Position(0, 2))
+        ant.carryingFood = true
+        ant.move()
+        ant.carryingFood should be(true)
+        ant.interactWithObject()
+        ant.carryingFood should be(true)
+    }   
 }

8c93032f2d588f9385662ad2bb652987cd7c75cb
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2024-06-19 10:45:45 +0200

    feat: implement builder pattern for particles

--- src/main/scala/pos/model/ParticleBuilder.scala
+++ src/main/scala/pos/model/ParticleBuilder.scala
@@ -4,10 +4,38 @@ import utils.Position
 import utils.Direction
 
 class ParticleBuilder:
-    def withId(id: Int): ParticleBuilder = ???
-    def withPosition(pos: Position): ParticleBuilder = ???
-    def withDirection(dir: Direction): ParticleBuilder = ???
-    def withVelocity(vel: Position): ParticleBuilder = ???
-    def withPersonalBest(personalBest: Position): ParticleBuilder = ???
-    def withGlobalBest(globalBest: Position): ParticleBuilder = ???
-    def build(): Particle = ???
+    private var id: Int = 0
+    private var pos: Position = Position(0, 0)
+    private var direction: Direction = Direction.Still
+    private var velocity: Position = Position(0, 0)
+    private var personalBest: Position = Position(0, 0)
+    private var globalBest: Position = Position(0, 0)
+    
+    def withId(id: Int): ParticleBuilder = 
+        this.id = id
+        this
+
+    def withPosition(pos: Position): ParticleBuilder = 
+        this.pos = pos
+        this
+
+    def withDirection(dir: Direction): ParticleBuilder = 
+        this.direction = dir
+        this
+
+    def withVelocity(vel: Position): ParticleBuilder = 
+        this.velocity = vel
+        this
+    
+    def withPersonalBest(personalBest: Position): ParticleBuilder = 
+        this.personalBest = personalBest
+        this
+    
+    def withGlobalBest(globalBest: Position): ParticleBuilder = 
+        this.globalBest = globalBest
+        this
+    
+    def build(): Particle = 
+        val state = State(pos, direction, velocity)
+        val best = Best(personalBest, globalBest)
+        Particle(id)(state, best)

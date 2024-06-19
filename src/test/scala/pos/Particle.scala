e92990fe7d13d32cd1af7f4169056aba2952b8e1
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2024-06-19 10:50:56 +0200

    chore: refactor test

--- src/test/scala/pos/Particle.scala
+++ src/test/scala/pos/Particle.scala
@@ -7,13 +7,21 @@ import utils.Direction
 import pos.model.ParticleBuilder
 
 class ParticleSpec extends AnyFlatSpec with Matchers {
+
+    val particle = ParticleBuilder()
+        .withId(1)
+        .withPosition(Position(0, 0))
+        .withDirection(Direction.East)
+        .withVelocity(Position(1, 0))
+        .withPersonalBest(Position(2, 2))
+        .withGlobalBest(Position(3, 3))
+        .build()
+
     "A Particle" should "be created correctly" in {
-        val particle = ParticleBuilder()
-            .withId(1)
-            .withPosition(Position(0, 0))
-            .withDirection(Direction.North)
-            .build()
         particle.pos should be(Position(0, 0))
-        particle.dir should be(Direction.North)
+        particle.dir should be(Direction.East)
+        particle.state.velocity should be(Position(1, 0))
+        particle.best.personalBest should be(Position(2, 2))
+        particle.best.globalBest should be(Position(3, 3))
     }
 }

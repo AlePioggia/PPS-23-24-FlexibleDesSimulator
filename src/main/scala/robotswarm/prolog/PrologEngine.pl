b09859b5698c782e7279a27f0bd2adb1ff73382c
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2024-08-24 15:18:11 +0200

    feat: generate matrix dinamically in prolog

--- src/main/scala/robotswarm/prolog/PrologEngine.pl
+++ src/main/scala/robotswarm/prolog/PrologEngine.pl
@@ -1,11 +1,3 @@
-matrix([
-  [0, 1, 0, 0, 0],
-  [0, 1, 0, 1, 0],
-  [0, 0, 0, 1, 0],
-  [0, 1, 1, 1, 0],
-  [0, 0, 0, 0, 0]
-]).
-
 % Movement directions: down, up, right, left
 direction((1, 0)).
 direction((-1, 0)).
--- src/main/scala/robotswarm/prolog/PrologEngine.scala
+++ src/main/scala/robotswarm/prolog/PrologEngine.scala
@@ -17,17 +17,35 @@ object PrologEngine {
     content
   }
 
-  def testAStar(startPos: Position, goal: Position): Unit =
+  def matrixToPrologString(matrix: Array[Array[scala.Int]]): String = {
+    matrix.map(row => "[" + row.mkString(",") + "]").mkString(",")
+  }
+
+  def testAStar(matrix: Array[Array[scala.Int]], startPos: Position, goal: Position): Unit =
+    var matrixString = matrixToPrologString(matrix)
+    println(matrixString)
     val query = s"find_path((0,0), (4,4), Path)."
     val solution: SolveInfo = engine.solve(query)
-    print(solution)
 
   // main to test
   def main(args: Array[String]): Unit = {
-    engine.addTheory(new Theory(theoryString))
+    val matrix: Array[Array[scala.Int]] = Array(
+      Array(0, 0, 0, 0, 0),
+      Array(0, 1, 1, 1, 0),
+      Array(0, 1, 0, 1, 0),
+      Array(0, 1, 1, 1, 0),
+      Array(0, 0, 0, 0, 0)
+    )
+
+    var mString = "matrix([" + matrixToPrologString(matrix) + "])."
+    var newTheoryString = mString + "\n" + theoryString
+    
+    engine.addTheory(new Theory(mString + "\n" + theoryString))
+
     val startPos = Position(0, 0)
     val goal = Position(4, 4)
-    testAStar(startPos, goal)
+
+    testAStar(matrix, startPos, goal)
   }
 }
 
